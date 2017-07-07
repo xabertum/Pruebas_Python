459d555a912abf5e3b210f80148e9b103029770a
xabertum <mohatar@gmail.com>
2017-07-07 21:37:38 +0200

    added file

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,8 @@
-print ("hello world")
+def pedirNombre():
+    name = raw_input('Introduce el nombre: ')
+    return name
+
+def imprimirNombre(nombre):
+    print 'el nombre introducido es... ' + nombre
+
+imprimirNombre(pedirNombre())
