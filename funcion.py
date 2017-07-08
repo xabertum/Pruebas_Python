587f9db4f7dd57527e040d9723fa3bac3c994905
xabertum <mohatar@yahoo.com>
2017-07-08 11:15:51 +0200

    def added

--- funcion.py
+++ funcion.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 def saludar(nombre, mensaje):
     print nombre, mensaje
 
@@ -7,6 +9,12 @@ saludar('Javier', 'Hola');
 def preguntarEdad():
     edad = raw_input('Introduce tu edad...:')
     if (edad > 35):
-        print 'Enhorabuena!';
+        print 'Enhorabuena!'
+    return edad;
 
-preguntarEdad()
+
+def calcularAnio():
+    date = 2017 - int(preguntarEdad())
+    print 'Tu año de nacimiento fue el: ' + str(date)
+
+calcularAnio()
