e244de8b5120cd597d5fc891af76958801f701ec
xabertum <mohatar@gmail.com>
2017-07-08 11:03:37 +0200

    minor changes

--- prueba1.py
+++ prueba1.py
@@ -2,7 +2,9 @@ def pedirNombre():
     name = raw_input('Introduce el nombre: ')
     return name
 
+
 def imprimirNombre(nombre):
     print 'el nombre introducido es... ' + nombre
+
 
 imprimirNombre(pedirNombre())
