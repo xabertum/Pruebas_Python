253d208060d780bc4566b511a1b73bbea9966362
xabertum <mohatar@gmail.com>
2017-07-10 00:14:14 +0200

    added file

---
+++ mayorMenosMenor.py
@@ -0,0 +1,10 @@
+
+num1 = raw_input('Introduce el numero A: ')
+num2 = raw_input('Introduce el numero B: ')
+
+if num1 > num2:
+    resultado = num1 -num2
+else:
+    resultado = num2 - num1
+
+print resultado
