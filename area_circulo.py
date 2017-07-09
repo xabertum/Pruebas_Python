9106850f39fe3150b6570b7405565ab86a9eca97
Javier Delgado <Javier Delgado>
2017-07-09 21:45:18 +0200

    continue

--- area_circulo.py
+++ area_circulo.py
@@ -5,9 +5,15 @@ def pedirRadio():
 	radio = int(raw_input('Introduce el radio: '))
 	return radio
 
+
 def calcularArea():
-	area = pedirRadio() ** math.pi
+	radio = pedirRadio()
+	area = round (math.pow(radio, 2) * math.pi ,2)
 	print 'El area del circulo es: ' + str(area)
+
+	circun = 2 * math.pi * radio
+	print 'La circunferencia es: ' + str(circun)
+
 
 calcularArea()
 
