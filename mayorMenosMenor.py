bce2f6d5b4c79639aad76bb08c33be3e8fe54987
xabertum <mohatar@gmail.com>
2017-07-10 00:15:59 +0200

    minor changes

--- mayorMenosMenor.py
+++ mayorMenosMenor.py
@@ -1,6 +1,6 @@
 
-num1 = raw_input('Introduce el numero A: ')
-num2 = raw_input('Introduce el numero B: ')
+num1 = float(raw_input('Introduce el numero A: '))
+num2 = float(raw_input('Introduce el numero B: '))
 
 if num1 > num2:
     resultado = num1 -num2
