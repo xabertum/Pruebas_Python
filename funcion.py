298f35823a21e63c56af9eacc29880b99cdd4832
xabertum <mohatar@gmail.com>
2017-07-09 20:55:33 +0200

    minor changes

--- funcion.py
+++ funcion.py
@@ -1,12 +1,26 @@
 def saludar(nombre, mensaje):
     print nombre, mensaje
 
-saludar('Javier', 'Hola');
-
 
 def preguntarEdad():
-    edad = raw_input('Introduce tu edad...:')
+    edad = int(raw_input('Introduce tu edad...:'))
     if (edad > 35):
         print 'Enhorabuena!';
+   
+    return edad
 
-preguntarEdad()
+
+def calcularFecha():
+   date = 2017 - preguntarEdad()
+   print('Tu fecha de nacimiento es: ' + str(date))
+
+
+def quiereSeguirJugando():
+	respuesta = raw_input('Quiere seguir jugando: ')
+	if (respuesta == 's'):
+		calcularFecha()
+
+
+saludar('Javier', 'Bienvenido a ti mismo')
+calcularFecha()
+quiereSeguirJugando()
