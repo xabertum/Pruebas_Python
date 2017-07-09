f1823a702ac66dcede01c6133dabacf534915a07
xabertum <mohatar@gmail.com>
2017-07-09 23:36:20 +0200

    without def

--- positivonegativo.py
+++ positivonegativo.py
@@ -2,17 +2,11 @@
 
 esPositivo = True
 
+num = int(raw_input('Introduce el numero:'))
 
-def pedirNumero():
-    num = int(raw_input('Introduce el numero:'))
+if num < 0:
+    esPositivo = False
 
-    if num < 0:
-        esPositivo = False
-
-
-def toPrint():
-    print 'el numero es positivo: ' + str(esPositivo)
+print 'el numero es positivo: ' + str(esPositivo)
 
 
-pedirNumero()
-toPrint()
