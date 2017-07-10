0387a1b7ea2df1cf90ca07877ec5fdebf3ed8398
xabertum <mohatar@gmail.com>
2017-07-10 08:45:53 +0200

    continue

--- funcion.py
+++ funcion.py
@@ -7,7 +7,6 @@ def saludar(nombre, mensaje):
 def preguntarEdad():
     edad = int(raw_input('Introduce tu edad...:'))
     if (edad > 35):
-<<<<<<< HEAD
         print 'Enhorabuena!';
    
     return edad
@@ -27,16 +26,3 @@ def quiereSeguirJugando():
 saludar('Javier', 'Bienvenido a ti mismo')
 calcularFecha()
 quiereSeguirJugando()
-=======
-        print 'Enhorabuena!'
-    return edad;
-
-
-def calcularAnio():
-    date = 2017 - int(preguntarEdad())
-    print 'Tu año de nacimiento fue el: ' + str(date)
-
-calcularAnio()
-
-probando...
->>>>>>> cbff3022cc156423abae39c80047f83e1281a5fa
