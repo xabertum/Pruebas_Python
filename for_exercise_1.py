ebf76090c3a32e04b78319f71ab198ba8414cb2e
xabertum <mohatar@gmail.com>
2017-07-19 20:34:42 +0200

    addel files

--- for_exercise_1.py
+++ for_exercise_1.py
@@ -1,7 +1,8 @@
+sum = 0
 
-for i in range(1,11):
+for i in range(1,10 + 1):
 
-    sum = sum + i
-
+    sum +=i
     print sum
+
 
--- iteracion1.py
+++ iteracion1.py
@@ -2,7 +2,7 @@ total = 25
 
 
 def prueba(total):
-    for i in range(0, 9 + 1):
+    for i in range(0, 9):
         total += 1
     print total
 
