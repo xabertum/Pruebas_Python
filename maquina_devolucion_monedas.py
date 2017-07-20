6bffe7c8bf1cf6102d9b41e855a8ee6c222cb59a
xabertum <mohatar@gmail.com>
2017-07-20 22:54:02 +0200

    addel files

--- maquina_devolucion_monedas.py
+++ maquina_devolucion_monedas.py
@@ -2,15 +2,3 @@
 
 import math
 
-cantidad = float(input('Introduce la cantidad a devolver: '))
-
-cantidad_monedas_euro = math.trunc(cantidad)
-i = 25
-
-def devolucion(cantidad, tipo):
-    for i in range(0, cantidad):
-        cantidad -= tipo
-        return i
-
-
-print devolucion(25, 1)
