59b367432978e08273d893af307da0c6e5fb6f43
xabertum <mohatar@gmail.com>
2017-07-21 20:44:12 +0200

    minor changes

--- conversor_datos.py
+++ conversor_datos.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
 
 cant_info = input('Introduce una cantidad de información')
-unidad_info = input('¿ En que unidad esta expresada ?: ')
+unidad_info = input('¿ En que unidad esta expresada ? (bit, byte, KB, MB o GB) : ')
+unidad_info_conv = input('A que unidad desesa convertir (bit, byte, KB, MB o GB)')
 
-def convertir(cant, unidad):
-    return {
-        'bit': cant
+def conversor(unidad_info, unidad_conv):
+    if unidad_info == unidad_conv:
+        pass
+    else:
+        if unidad_info == 'bit' and unidad_conv == 'byte':
 
 
 
 
-    }.get(unidad, 'Unidad no reconocida')
+
+
